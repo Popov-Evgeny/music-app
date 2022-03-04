93e47a303fecd9fb451c476e89cebe4c7eb74d79
Evgeny Popov <evgeny.popov.312@gmail.com>
2022-03-04 18:30:13 +0600

    create get request by album id, made an extra

--- api/routers/tracks.js
+++ api/routers/tracks.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Track = require('../models/Track');
+const Album = require("../models/Album");
+const Artist = require("../models/Artist");
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
  try {
    const query = {};
+   let artistsTracks = [];
+
    if (req.query.album){
-     query.author = {_id: req.query.album};
+     query.album = {_id: req.query.album};
+   }
+
+   if (req.query.artist) {
+         const albums = await Album.find({author: {_id: req.query.artist}});
+         for (let album of albums) {
+           artistsTracks= await Track.find({album: {_id: album._id}});
+         }
+         return res.send(artistsTracks);
    }
    const tracks = await Track.find(query).populate('album', 'name');
    return res.send(tracks);
@@ -19,6 +31,27 @@ router.get('/', async (req, res, next) => {
    return next(e);
  }
 });
+
+router.get('/byAlbum/:albumID', async (req,res,next) => {
+  try {
+    const albums = await Album.find({_id: req.params.albumID}).populate('author', 'name');
+    const artists = await Artist.find({_id: albums[0].author._id});
+    const tracks = await Track.find({album: {_id: albums[0]._id}}).populate('album', 'name');
+    const info = {
+      artist: artists[0],
+      album: albums[0],
+      track: tracks
+    }
+    return res.send(info);
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(e);
+    }
+    return next(e);
+  }
+
+});
+
 
 router.post('/', async (req, res, next) => {
   try {
